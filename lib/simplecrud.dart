08a382f0a88ee3209df95d7592a0fbc3ecbfa178
mintanlathiya <lathiyamintan1998@gmail.com>
2023-08-22 19:28:23 +0530

    first commit

---
+++ lib/simplecrud.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class SimpleCrudDemo extends StatefulWidget {
+  const SimpleCrudDemo({super.key});
+
+  @override
+  State<SimpleCrudDemo> createState() => _SimpleCrudDemoState();
+}
+
+class _SimpleCrudDemoState extends State<SimpleCrudDemo> {
+  TextEditingController nameController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            TextField(
+              controller: nameController,
+            ),
+            MaterialButton(
+              onPressed: () {},
+              child: const Text('submit'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
